cdf75a17eecbb5085a3f02b185e2f7c57282e26b
baynhamch <130159962+baynhamch@users.noreply.github.com>
2025-05-20 22:41:06 -0400

    Initial Commit

---
+++ creature/creatureApp.swift
@@ -0,0 +1,17 @@
+//
+//  creatureApp.swift
+//  creature
+//
+//  Created by Nicholas Conant-Hiley on 5/20/25.
+//
+
+import SwiftUI
+
+@main
+struct creatureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
